b39815025dc093fb7ed4cc3309ec9c3c1b2b5def
pho <songhapho@dal.ca>
2022-03-26 01:24:41 -0300

    Fixed the folder list

--- app/src/main/java/com/example/flashcards/fragments/HomepageFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/HomepageFragment.kt
@@ -1,7 +1,9 @@
 package com.example.flashcards.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -16,6 +18,18 @@ import com.example.flashcards.persistence.FolderPersistence
 import org.w3c.dom.Text
 
 class HomepageFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homepage, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //folder list
