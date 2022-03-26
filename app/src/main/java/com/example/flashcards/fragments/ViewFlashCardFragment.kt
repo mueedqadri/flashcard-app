91bd258e0d8d2cd521b825869e8a162ab5f462d8
Janhavi Sonawane <jn476959@dal.ca>
2022-03-26 09:28:39 -0300

    Collapse card on navigation and bug fix for hide answer of next/prev card on navigation

--- app/src/main/java/com/example/flashcards/fragments/ViewFlashCardFragment.kt
+++ app/src/main/java/com/example/flashcards/fragments/ViewFlashCardFragment.kt
@@ -39,13 +39,12 @@ class ViewFlashCardFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i("tagtag",requireArguments().getSerializable("currCard").toString())
+
+        //Get card list and index from card list view
         var position = requireArguments().getInt("currCardPosition")
         var allcard = (requireArguments().getSerializable("allCards")) as List<FlashCardModel>
-        Log.i("allflashcards",allcard.toString())
         var card = allcard.get(position);
-        Log.i("currentCard",card.toString());
-        var currentCard = requireArguments().getSerializable("currCard");
+
         val toolBar = view.findViewById<Toolbar>(R.id.topAppBarViewCardFragment)
         val viewCardQuestion = view.findViewById<TextView>(R.id.view_card_question)
         val viewCardAnswer = view.findViewById<TextView>(R.id.view_card_answer)
@@ -62,7 +61,7 @@ class ViewFlashCardFragment : Fragment() {
                 TransitionManager.beginDelayedTransition(cardView, AutoTransition())
                 layoutExpand.visibility = View.VISIBLE
                 viewAnswerButton.text = "Hide Answer"
-                viewCardAnswer.text = (card as FlashCardModel).answer;
+                viewCardAnswer.text = (allcard.get(position) as FlashCardModel).answer;
             }
             else{
                 TransitionManager.beginDelayedTransition(cardView, AutoTransition())
@@ -70,30 +69,57 @@ class ViewFlashCardFragment : Fragment() {
                 viewAnswerButton.text = "View Answer"
             }
         }
+
+        //On back click
         toolBar.setNavigationOnClickListener {
             findNavController().navigate(R.id.action_viewFlashCardFragment_to_notesListFragment)
         }
+
+        //Initial card visibility
         nextCardbutton.isVisible = allcard.size-1 != position
         previousCardbutton.isVisible = position != 0
         nextCardbutton.setOnClickListener{
+            viewCardAnswer.text = "";
+
+            //Collapse card answer
+            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+            layoutExpand.visibility = View.GONE
+            viewAnswerButton.text = "View Answer"
+
+            //Get next card position
             position++;
+
+            //Set card visibility
             Log.i("position in next",position.toString())
             nextCardbutton.isVisible = allcard.size-1 != position
             previousCardbutton.isVisible = position != 0
+
+            //Set Card Data
             var card = allcard[position]
             viewCardQuestion.text = (card as FlashCardModel).question;
-            viewCardAnswer.text = (card as FlashCardModel).answer;
+            Log.i("ANSWR",card.answer)
 
         }
         previousCardbutton.setOnClickListener{
+            viewCardAnswer.text = "";
+
+            //Collapsing answer on navigation
+            TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+            layoutExpand.visibility = View.GONE
+            viewAnswerButton.text = "View Answer"
+
+            //Get card from previous position
             position--;
             Log.i("position in previous",position.toString())
+
+            //Set card visibility
             previousCardbutton.isVisible = position != 0
             nextCardbutton.isVisible = allcard.size-1 != position
+
+            //Set card data
             var card1 = allcard[position]
             viewCardQuestion.text = (card1 as FlashCardModel).question;
-            viewCardAnswer.text = (card1 as FlashCardModel).answer;
-            //position--;
+
         }
 
     }
